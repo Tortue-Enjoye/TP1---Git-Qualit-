ca0ff5b47bf6644dce268df68bc67b8b900656c8
Tom Pottin <tom.pottin@gmail.com>
2026-09-09 13:53:14 +0200

    Réalistion de la feature add

---
+++ TP1-qualite/src/test/java/TP1_qualite/AddTest.java
@@ -0,0 +1,21 @@
+package TP1_qualite;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class AddTest 
+{
+    @Test
+    public void testAdd1(){
+        int a,b,r;
+
+        a = 1;
+        b = 1;
+
+        r = 2;
+
+        Assert.assertEquals("Echec test 1",r,Add.add(a, b));
+
+    }
+}
