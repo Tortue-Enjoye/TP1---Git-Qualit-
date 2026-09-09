59aa16c42fa93b3883e1fb75c14a468ab5616144
Tom Pottin <tom.pottin@gmail.com>
2026-09-09 14:17:57 +0200

    Soustraction fonctionnelle

--- TP1-qualite/src/main/java/TP1_qualite/Main.java
+++ TP1-qualite/src/main/java/TP1_qualite/Main.java
@@ -3,5 +3,6 @@ package TP1_qualite;
 public class Main {
     public static void main(){
         System.out.println("Calculatrice");
+        System.out.println("Soustraction fonctionnelle dans la calculatrice");
     }
 }
