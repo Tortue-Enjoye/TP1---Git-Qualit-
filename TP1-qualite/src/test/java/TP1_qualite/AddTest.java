daa75bd16c06e39b5e58acf75f0d6a8ab09a935d
Tom Pottin <tom.pottin@gmail.com>
2026-09-09 13:55:54 +0200

    Ajout des tests manquant

--- TP1-qualite/src/test/java/TP1_qualite/AddTest.java
+++ TP1-qualite/src/test/java/TP1_qualite/AddTest.java
@@ -18,4 +18,28 @@ public class AddTest
         Assert.assertEquals("Echec test 1",r,Add.add(a, b));
 
     }
+
+    @Test
+    public void testAdd2(){
+        int a,b,r;
+
+        a = -1;
+        b = -1;
+
+        r = -2;
+
+        Assert.assertEquals("Echec test 2",r,Add.add(a, b));
+    }
+
+    @Test
+    public void testAdd3(){
+        int a,b,r;
+
+        a = -1;
+        b = 1;
+
+        r = 0;
+
+        Assert.assertEquals("Echec test 3",r,Add.add(a, b));
+    }
 }
