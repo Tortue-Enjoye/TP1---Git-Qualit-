a8e178980766fb17f7f912a7ffeae1dd53fdf9c2
Tom.pottin <tom.pottin@gmail.com>
2026-09-09 14:16:23 +0200

    Addition fonctionnel

--- TP1-qualite/src/main/java/TP1_qualite/Main.java
+++ TP1-qualite/src/main/java/TP1_qualite/Main.java
@@ -3,5 +3,6 @@ package TP1_qualite;
 public class Main {
     public static void main(){
         System.out.println("Calculatrice");
+        System.out.println("Addition fonctionnelle dans la calculatrice");
     }
 }
