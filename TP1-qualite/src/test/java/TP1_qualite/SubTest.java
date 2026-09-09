1a97f875c316d7f689c366fcfa628033e44e29c0
Tom Pottin <tom.pottin@gmail.com>
2026-09-09 13:46:18 +0200

    Ajout des différents tests demandé

--- TP1-qualite/src/test/java/TP1_qualite/SubTest.java
+++ TP1-qualite/src/test/java/TP1_qualite/SubTest.java
@@ -19,4 +19,29 @@ public class SubTest
 
     }
 
+    @Test
+    public void testSub2(){
+        int a,b,r;
+
+        a = 0;
+        b = 1;
+
+        r = -1;
+
+        Assert.assertEquals("Echec test 2",r,Sub.sub(a, b));
+
+    }
+
+    @Test
+    public void testSub3(){
+        int a,b,r;
+
+        a = 15;
+        b = 5;
+
+        r = 10;
+
+        Assert.assertEquals("Echec test 3",r,Sub.sub(a, b));
+
+    }
 }
